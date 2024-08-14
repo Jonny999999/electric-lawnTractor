e1de1701cc82f4aec2bbbe3d22cfa47830c97f8e
jonny <jonny@wwad.de>
2024-08-14 09:34:42 +0200

    Add 10Bit Fast-PWM, Add adc

---
+++ throttle-tune/fw_throttle-tune/a2d.h
@@ -0,0 +1,15 @@
+#include <util/delay.h>
+//#include "bsp_io.h"
+//Vcc = 1023
+//GND = 0
+//
+//Atmega8:
+//PC5 = ADC5 = Channel 5
+//PC4 = ADC4 = Channel 4
+//PC3 = ADC2 = Channel 3
+//PC2 = ADC2 = Channel 2
+//PC1 = ADC1 = Channel 1
+//PC0 = ADC0 = Channel 0
+
+//example: value = ReadChannel(5);
+uint16_t ReadChannel(uint8_t mux);
--- throttle-tune/fw_throttle-tune/main.c
+++ throttle-tune/fw_throttle-tune/main.c
@@ -1,12 +1,44 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "usart.h"
+#include "a2d.h"
+
+
+
+
+
+void Timer1_FastPWM_Init(void) {
+  //Mode 7 Datasheet page 98 Table 39
+    // Set Fast PWM mode with 10-bit resolution
+    TCCR1A = (1 << WGM10) | (1 << WGM11); // Set WGM10 and WGM11 for 10-bit Fast PWM
+    TCCR1B = (1 << WGM12) | (1 << CS10);  // Set WGM12, no prescaler
+
+    // Set non-inverting mode
+    TCCR1A |= (1 << COM1A1);  // Clear OC1A on Compare Match, set OC1A at BOTTOM
+    // If using OCR1B, uncomment the next line
+    // TCCR1A |= (1 << COM1B1);  // Clear OC1B on Compare Match, set OC1B at BOTTOM
+
+    // Set PB1/OC1A as output (for OC1A PWM)
+    DDRB |= (1 << PB1);
+    // If using OC1B, set PB2 as output
+    // DDRB |= (1 << PB2);
+}
+
+void Set_PWM_Duty_Cycle(uint16_t duty_cycle) {
+    OCR1A = duty_cycle;  // Set duty cycle for OC1A
+    // If using OCR1B, set duty cycle for OC1B
+    // OCR1B = duty_cycle;
+}
+
+
+
 
 
 
 
 int main(void)
 {
+  Timer1_FastPWM_Init(); // Initialize Timer1 in Fast PWM mode with 10Bit resolution on Pin PB1
 
   uart_init();
 
@@ -15,17 +47,29 @@ int main(void)
 
   uart_sendStr("hello world\n");
 
-int count = 0;
+  uint16_t duty = 0;
 
   while (1)
   {
-    printf("count=%03d\n", count++);
+    // read adc
+    // max: 1023 = 5V
+    uint16_t adcInput = ReadChannel(5); //PC5
+    uint16_t adcOutput = ReadChannel(4); //PC4  - measure generated output voltage for debugging
 
+    // logging
+    printf("adcIn=%04d, adcOut=%04d, duty=%04d\n", adcInput, adcOutput, duty);
+
+    // test pwm
+    duty += 50; // increment
+    duty %= 1023; //limit to range
+    Set_PWM_Duty_Cycle(duty); // Example: Set duty cycle to 50%
+
+    // blink led
     // Turn on PC4
     PORTC |= (1 << PC4);
-    _delay_ms(100);
+    _delay_ms(50);
     // Turn off PC4
     PORTC &= ~(1 << PC4);
-    _delay_ms(100);
+    _delay_ms(50);
   }
 }

64b9bcaf53720c44087d2916d860352bc48b35d5
jonny <jonny@wwad.de>
2024-08-15 09:44:37 +0200

    Add throttle scaling (works)

--- throttle-tune/fw_throttle-tune/main.c
+++ throttle-tune/fw_throttle-tune/main.c
@@ -34,16 +34,28 @@ void Set_PWM_Duty_Cycle(uint16_t duty_cycle) {
 
 
 
+// config
+#define GAS_PEDAL_MAX 750
+#define GAS_PEDAL_MIN 200 // actual 172
+
+#define CONTROLLER_START 250
+#define CONTROLLER_MAX 1023
+
+#define LEVEL1_MAX_PERCENT 6
+
+
+// calculations
+#define LEVEL1_MAX_DUTY (CONTROLLER_MAX - CONTROLLER_START) * LEVEL1_MAX_PERCENT / 100  + CONTROLLER_START
 
 
 int main(void)
 {
-  Timer1_FastPWM_Init(); // Initialize Timer1 in Fast PWM mode with 10Bit resolution on Pin PB1
+  Timer1_FastPWM_Init(); // init timer1 in fast PWM mode with 10bit resolution on pin PB1
 
   uart_init();
 
-  // Define PC4 as output
-  DDRC |= (1 << PC4);
+  // Define PC3 as output
+  DDRC |= (1 << PC3);
 
   uart_sendStr("hello world\n");
 
@@ -53,23 +65,43 @@ int main(void)
   {
     // read adc
     // max: 1023 = 5V
-    uint16_t adcInput = ReadChannel(5); //PC5
+    // read gas pedal
+    uint16_t adcInputGasPedal = ReadChannel(5); //PC5
+    if (adcInputGasPedal < GAS_PEDAL_MIN) adcInputGasPedal = GAS_PEDAL_MIN;
+    if (adcInputGasPedal > GAS_PEDAL_MAX) adcInputGasPedal = GAS_PEDAL_MAX;
+    // read output (generated analog voltage via pwm)
     uint16_t adcOutput = ReadChannel(4); //PC4  - measure generated output voltage for debugging
 
-    // logging
-    printf("adcIn=%04d, adcOut=%04d, duty=%04d\n", adcInput, adcOutput, duty);
+    // calculate percentages for logging
+    uint16_t pedalPercent = (uint32_t)(adcInputGasPedal-GAS_PEDAL_MIN) * 100 / (GAS_PEDAL_MAX - GAS_PEDAL_MIN);
 
-    // test pwm
-    duty += 50; // increment
-    duty %= 1023; //limit to range
-    Set_PWM_Duty_Cycle(duty); // Example: Set duty cycle to 50%
+    uint8_t motorPercent;
+    //uint8_t motorPercent = (duty-CONTROLLER_START) * 100 / (CONTROLLER_MAX - CONTROLLER_START);
+    if (adcOutput < CONTROLLER_START) motorPercent = 0;
+    else motorPercent = (adcOutput-CONTROLLER_START) * 100 / (CONTROLLER_MAX - CONTROLLER_START);
+
+    // logging
+    printf("adcIn=%04d, adcOut=%04d, duty=%04d -- pedalPercent=%2d, motorPercent=%2d\n", 
+    adcInputGasPedal, adcOutput, duty, pedalPercent, motorPercent);
+
+
+
+    // calculate duty
+    //TODO limit to range 0-1023   in case of negative = large number?!
+    duty = (uint32_t)(LEVEL1_MAX_DUTY-CONTROLLER_START) *1000 / (GAS_PEDAL_MAX - GAS_PEDAL_MIN)  * (adcInputGasPedal - GAS_PEDAL_MIN) / 1000 + CONTROLLER_START;
+
+  //turn off completely when below controller start threshold
+    if (duty <= CONTROLLER_START) duty = 0;
+
+  // apply new duty
+    Set_PWM_Duty_Cycle(duty);
 
     // blink led
     // Turn on PC4
-    PORTC |= (1 << PC4);
-    _delay_ms(50);
-    // Turn off PC4
-    PORTC &= ~(1 << PC4);
-    _delay_ms(50);
+    //PORTC |= (1 << PC3);
+    //_delay_ms(50);
+    //// Turn off PC4
+    //PORTC &= ~(1 << PC3);
+    //_delay_ms(50);
   }
 }
